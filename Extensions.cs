4329684d168f7b343d914cd4b1f4facd6cad484e
Zsolt <5047426+bodzaital@users.noreply.github.com>
2020-09-17 08:07:33 +0200

    Even more fix: turns out you can't just copy from another project.

--- Extensions.cs
+++ Extensions.cs
@@ -10,15 +10,22 @@ static class Extensions
     /// <returns></returns>
     public static Polar2 ToPolar2(this Vector3 v, Transform sphere)
     {
-        Vector3 rotated = v - sphere.position;
-        rotated = Quaternion.Euler(sphere.rotation.eulerAngles.x, sphere.rotation.eulerAngles.y, sphere.rotation.eulerAngles.z) * rotated;
+        Vector3 pos = v - sphere.position;
 
-        Vector3 azimuthReference = Quaternion.Euler(sphere.rotation.eulerAngles.x, sphere.rotation.eulerAngles.y, sphere.rotation.eulerAngles.z) * new Vector3(0, 0, -1 *sphere.localScale.z);
+        Vector3 azimuthReference = Quaternion.Euler(sphere.rotation.eulerAngles.x, sphere.rotation.eulerAngles.y, sphere.rotation.eulerAngles.z) * new Vector3(0, 0, -1 * (sphere.localScale.z / 2));
         //Vector3 rightAzimuthReference = Quaternion.Euler(sphere.rotation.eulerAngles.x, sphere.rotation.eulerAngles.y, sphere.rotation.eulerAngles.z) * new Vector3(sphere.localScale.x, 0, 0);
 
+        Debug.DrawLine(sphere.position, azimuthReference);
+
+        Vector3 zenithReference = Quaternion.Euler(sphere.rotation.eulerAngles.x, sphere.rotation.eulerAngles.y, sphere.rotation.eulerAngles.z) * (Vector3.up * (sphere.localScale.x / 2));
+
+        Debug.DrawLine(sphere.position, zenithReference);
+
+        Debug.DrawLine(sphere.position, pos, Color.green);
+
         Polar2 polar = new Polar2(
-            Vector3.SignedAngle(Vector3.up, rotated, Vector3.up),
-            Vector3.SignedAngle(azimuthReference, Vector3.ProjectOnPlane(v - sphere.position, Vector3.up), Vector3.up));
+            Vector3.Angle(zenithReference, pos),
+            Vector3.SignedAngle(azimuthReference, Vector3.ProjectOnPlane(v - sphere.position, zenithReference), zenithReference));
 
         return polar;
     }
