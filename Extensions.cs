0e17d7697e1844d5b874e419633afba4271d44e0
Zsolt <5047426+bodzaital@users.noreply.github.com>
2020-09-17 08:08:11 +0200

    Remove Debug.Drawline

--- Extensions.cs
+++ Extensions.cs
@@ -15,13 +15,7 @@ static class Extensions
         Vector3 azimuthReference = Quaternion.Euler(sphere.rotation.eulerAngles.x, sphere.rotation.eulerAngles.y, sphere.rotation.eulerAngles.z) * new Vector3(0, 0, -1 * (sphere.localScale.z / 2));
         //Vector3 rightAzimuthReference = Quaternion.Euler(sphere.rotation.eulerAngles.x, sphere.rotation.eulerAngles.y, sphere.rotation.eulerAngles.z) * new Vector3(sphere.localScale.x, 0, 0);
 
-        Debug.DrawLine(sphere.position, azimuthReference);
-
         Vector3 zenithReference = Quaternion.Euler(sphere.rotation.eulerAngles.x, sphere.rotation.eulerAngles.y, sphere.rotation.eulerAngles.z) * (Vector3.up * (sphere.localScale.x / 2));
-
-        Debug.DrawLine(sphere.position, zenithReference);
-
-        Debug.DrawLine(sphere.position, pos, Color.green);
 
         Polar2 polar = new Polar2(
             Vector3.Angle(zenithReference, pos),
